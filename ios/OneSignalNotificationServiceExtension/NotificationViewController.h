457ad9dd5d56ecb04f971726f8899a5539e3d562
nadchif <nadchif@gmail.com>
2020-03-03 14:18:27 +0200

    added OneSignal for iOS

---
+++ ios/OneSignalNotificationServiceExtension/NotificationViewController.h
@@ -0,0 +1,13 @@
+//
+//  NotificationViewController.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Daniel Chif on 3/3/20.
+//  Copyright © 2020 The Chromium Authors. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NotificationViewController : UIViewController
+
+@end
